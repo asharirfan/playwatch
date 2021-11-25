fe2e3986a6f6fdc199fff2e9a5de774aa9aaf3e2
Ashar Irfan <mrasharirfan@gmail.com>
2021-11-26 02:29:48 +0500

    Improve homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
-import config from '@/utils/config';
 import Layout from '@/components/common/Layout';
 import PropTypes from 'prop-types';
+import { getTrendingMoviesByDay } from '@/functions/getTrendingMovies';
+import MoviesList from '@/components/movies/MoviesList';
+import TvShowsList from '@/components/tvshows/TvShowsList';
+import { getTrendingTvShowsByDay } from '@/functions/getTrendingTvShows';
 
 /**
  * Home page.
@@ -14,40 +16,8 @@ import PropTypes from 'prop-types';
 export default function Home({ movies, tvShows }) {
 	return (
 		<Layout>
-			<div id="trending-movies">
-				{movies.results.length > 0 ? (
-					movies.results.map(movie => {
-						return (
-							<article key={movie.id}>
-								<h3>{movie.title}</h3>
-								<p>{movie.overview}</p>
-								<p>Release Date: {movie.release_date}</p>
-							</article>
-						);
-					})
-				) : (
-					<article>
-						<p>No movies found.</p>
-					</article>
-				)}
-			</div>
-			<div id="trending-tv-shows">
-				{tvShows.results.length > 0 ? (
-					tvShows.results.map(tvShow => {
-						return (
-							<article key={tvShow.id}>
-								<h3>{tvShow.name}</h3>
-								<p>{tvShow.overview}</p>
-								<p>First aired on: {tvShow.first_air_date}</p>
-							</article>
-						);
-					})
-				) : (
-					<article>
-						<p>No tv shows found.</p>
-					</article>
-				)}
-			</div>
+			<MoviesList movies={movies.results} />
+			<TvShowsList tvShows={tvShows.results} />
 		</Layout>
 	);
 }
@@ -58,14 +28,10 @@ export default function Home({ movies, tvShows }) {
  * @return {object} Props object.
  */
 export async function getStaticProps() {
-	const moviesResponse = await axios(config.trendingMoviesByDay);
-
-	const tvShowsResponse = await axios(config.trendingTvShowsByDay);
-
 	return {
 		props: {
-			movies: moviesResponse.data,
-			tvShows: tvShowsResponse.data
+			movies: await getTrendingMoviesByDay(),
+			tvShows: await getTrendingTvShowsByDay()
 		}
 	};
 }
