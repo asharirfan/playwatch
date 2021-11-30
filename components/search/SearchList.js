697d10761f752dcd69bfe81e667bee3da72c3ab8
Ashar Irfan <mrasharirfan@gmail.com>
2021-11-30 23:39:49 +0500

    Improve SearchList to only show movies and tv results

--- components/search/SearchList.js
+++ components/search/SearchList.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import SearchResult from '@/components/search/SearchResult';
 
+const ALLOWED_MEDIA_TYPES = ['movie', 'tv'];
+
 /**
  * Search list component.
  *
@@ -12,9 +14,13 @@ export default function SearchList({ results }) {
 	return (
 		<div id="search-results">
 			{results && results.length > 0 ? (
-				results.map(result => (
-					<SearchResult key={result.id} result={result} />
-				))
+				results.map(result => {
+					if (ALLOWED_MEDIA_TYPES.indexOf(result.media_type) === -1) {
+						return;
+					}
+
+					return <SearchResult key={result.id} result={result} />;
+				})
 			) : (
 				<article>
 					<p>No results found.</p>
