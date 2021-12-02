0b9b9d5b24f1a444e45a9f272a62327cd5f6888a
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-03 01:59:03 +0500

    Add function to get first 10 pages to trending tv shows

--- functions/getTrendingTvShows.js
+++ functions/getTrendingTvShows.js
@@ -5,7 +5,7 @@ import axios from 'axios';
  * Get trending tv shows by day.
  *
  * @param  {number}      page Number of page. Default is 1.
- * @return {null|object}      Movies object or null if failed.
+ * @return {null|object}      TV shows object or null if failed.
  */
 async function getTrendingTvShowsByDay(page = 1) {
 	let tvShowsResponse = null;
@@ -21,4 +21,27 @@ async function getTrendingTvShowsByDay(page = 1) {
 	return tvShowsResponse ? tvShowsResponse.data : { results: [] };
 }
 
-export { getTrendingTvShowsByDay };
+/**
+ * Get first 50 pages of trending tv shows by day.
+ *
+ * @return {Array} TV shows array.
+ */
+async function getAllTrendingTvShowsByDay() {
+	let currentPage = 1;
+	let tvShows = [];
+	let tvShowsResponse = null;
+
+	do {
+		tvShowsResponse = await getTrendingTvShowsByDay(currentPage);
+
+		if (tvShowsResponse.results.length !== 0) {
+			tvShows = [...tvShows, ...tvShowsResponse.results];
+		}
+
+		currentPage = tvShowsResponse.page + 1;
+	} while (tvShowsResponse.page < 10);
+
+	return tvShows;
+}
+
+export { getTrendingTvShowsByDay, getAllTrendingTvShowsByDay };
