0104ea1def8f6f01c72ca560d125ad1dc73e5058
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-03 01:59:35 +0500

    Add link to single tv show component

--- components/tvshows/TvShow.js
+++ components/tvshows/TvShow.js
@@ -1,6 +1,7 @@
 import config from '@/utils/config';
 import Image from 'next/image';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
 /**
  * Single tv show component.
@@ -11,17 +12,21 @@ import PropTypes from 'prop-types';
  */
 export default function TvShow({ tvShow }) {
 	return (
-		<article key={tvShow.id}>
-			<Image
-				alt={tvShow.name}
-				height="278"
-				src={`${config.tmdbImgBaseUrl}w185${tvShow.poster_path}`}
-				width="185"
-			/>
-			<h3>{tvShow.name}</h3>
-			<p>{tvShow.overview}</p>
-			<p>{tvShow.first_air_date}</p>
-		</article>
+		<Link href={`/${tvShow.media_type}/${tvShow.id}`}>
+			<a>
+				<article key={tvShow.id}>
+					<Image
+						alt={tvShow.name}
+						height="278"
+						src={`${config.tmdbImgBaseUrl}w185${tvShow.poster_path}`}
+						width="185"
+					/>
+					<h3>{tvShow.name}</h3>
+					<p>{tvShow.overview}</p>
+					<p>{tvShow.first_air_date}</p>
+				</article>
+			</a>
+		</Link>
 	);
 }
 
