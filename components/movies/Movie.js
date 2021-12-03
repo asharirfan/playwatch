6f64a25d920cea8fb537c52e6bd56e555b558bcf
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-03 20:21:45 +0500

    Improve movie component to include link

--- components/movies/Movie.js
+++ components/movies/Movie.js
@@ -1,6 +1,7 @@
 import config from '@/utils/config';
 import Image from 'next/image';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
 /**
  * Single movie component.
@@ -10,18 +11,26 @@ import PropTypes from 'prop-types';
  * @return {Element}             HTML element.
  */
 export default function Movie({ movie }) {
+	const posterPath = movie.poster_path
+		? `${config.tmdbImgBaseUrl}w185${movie.poster_path}`
+		: 'https://via.placeholder.com/185x278.png/353849/03CC90?text=PlayWatch';
+
 	return (
-		<article key={movie.id}>
-			<Image
-				alt={movie.title}
-				height="278"
-				src={`${config.tmdbImgBaseUrl}w185${movie.poster_path}`}
-				width="185"
-			/>
-			<h3>{movie.title}</h3>
-			<p>{movie.overview}</p>
-			<p>{movie.release_date}</p>
-		</article>
+		<Link href={`/${movie.media_type}/${movie.id}`}>
+			<a>
+				<article key={movie.id}>
+					<Image
+						alt={movie.title}
+						height="278"
+						src={posterPath}
+						width="185"
+					/>
+					<h3>{movie.title}</h3>
+					<p>{movie.overview}</p>
+					<p>{movie.release_date}</p>
+				</article>
+			</a>
+		</Link>
 	);
 }
 
