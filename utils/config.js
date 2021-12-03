f308bd61f6c9f2444990b3286c36cd5c5c19e2a3
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-04 00:52:10 +0500

    Add person API endpoints to config

--- utils/config.js
+++ utils/config.js
@@ -7,8 +7,11 @@ module.exports = {
 	tmdbImgBaseUrl: 'https://image.tmdb.org/t/p/',
 	trendingMoviesByDay: `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
 	trendingMoviesByWeek: `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
+	trendingPersonsByDay: `https://api.themoviedb.org/3/trending/person/day?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
+	trendingPersonsByWeek: `https://api.themoviedb.org/3/trending/person/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
 	trendingTvShowsByDay: `https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
 	trendingTvShowsByWeek: `https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
 	getMovie: `https://api.themoviedb.org/3/movie`,
+	getPerson: `https://api.themoviedb.org/3/person`,
 	getTvShow: `https://api.themoviedb.org/3/tv`
 };
