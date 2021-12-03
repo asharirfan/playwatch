4676c6ec94cfee2b9c3a786a622ea015da4ea649
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-03 21:04:13 +0500

    Improve app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '@/styles/globals.css';
 
+/**
+ * Next.js App.
+ *
+ * @param  {object}  props           The component attributes as props.
+ * @param  {Element} props.Component React component to render as App.
+ * @param  {object}  props.pageProps App props.
+ * @return {Element}                 App.
+ */
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
