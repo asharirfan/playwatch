c300c61ef32f51f545d3bbd646e2f537d6fe96a8
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-07 01:45:11 +0500

    Improve header layout and classes

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
+import Menu from '@/components/menu';
 
 /**
  * Header component.
@@ -44,34 +45,44 @@ export default function Header() {
 	}
 
 	return (
-		<header>
-			<Link href="/">
-				<a>
-					<Image
-						src="/tmdb-logo.svg"
-						alt="Site Logo"
-						width={250}
-						height={70}
-					/>
-				</a>
-			</Link>
-			<div>
-				<Link href="/">Home</Link>
-				<Link href="/movie">Movies</Link>
-				<Link href="/tv">TV Shows</Link>
-				<Link href="/person">People</Link>
-			</div>
-			<div>
-				<form onSubmit={redirectOnSubmit}>
-					<input
-						id="search"
-						onChange={handleSearchQuery}
-						required
-						type="text"
-						value={searchQuery}
-					/>
-					<button type="submit">Search</button>
-				</form>
+		<header className="relative bg-header-bg">
+			<div className="absolute top-0 h-screen w-full bg-darkGrey bg-opacity-60 z-0" />
+			<div className="relative h-screen z-10">
+				<div className="container mx-auto flex flex-auto justify-between items-center">
+					<Link href="/">
+						<a className="cursor-pointer">
+							<Image
+								src="/tmdb-logo.svg"
+								alt="Site Logo"
+								width={250}
+								height={70}
+							/>
+						</a>
+					</Link>
+					<Menu />
+				</div>
+				<div className="container mx-auto flex flex-auto justify-center items-center h-full">
+					<form
+						className="text-center w-1/2"
+						onSubmit={redirectOnSubmit}
+					>
+						<input
+							className="text-text bg-white px-4 py-2 w-3/4 text-lg text-left"
+							id="search"
+							onChange={handleSearchQuery}
+							required
+							type="text"
+							value={searchQuery}
+							placeholder="Search for a movie, tv show..."
+						/>
+						<button
+							className="text-text bg-green text-lg px-4 py-2"
+							type="submit"
+						>
+							Search
+						</button>
+					</form>
+				</div>
 			</div>
 		</header>
 	);
