fafe1b8513f5f271e6355dc3f87ed38c7b75564b
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-09 21:28:48 +0500

    Improve button component to include classnames

--- components/button/index.js
+++ components/button/index.js
@@ -1,13 +1,17 @@
 /**
  * Simple button component.
  *
- * @param  {object}  props          The component attributes as props.
- * @param  {Element} props.children Component children to render.
- * @return {Element}                Button.
+ * @param  {object}  props           The component attributes as props.
+ * @param  {Element} props.children  Component children to render.
+ * @param  {string}  props.className Additional classnames.
+ * @return {Element}                 Button.
  */
-export default function Button({ children }) {
+export default function Button({ children, className }) {
 	return (
-		<button className="text-text bg-green text-lg px-4 py-2" type="submit">
+		<button
+			className={`text-text bg-green text-lg px-4 py-2 ${className}`}
+			type="submit"
+		>
 			{children}
 		</button>
 	);
