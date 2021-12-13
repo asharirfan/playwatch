58b60c26bdc6dfdd6ef8b74c4baccd8215c76f8c
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-14 02:19:19 +0500

    Improve run time function

--- functions/getRuntime.js
+++ functions/getRuntime.js
@@ -1,11 +1,11 @@
 /**
- * Get movie runtime in hrs and minutes.
+ * Get runtime in hrs and minutes.
  *
  * @param  {number} minutes Number of minutes.
  * @return {string}         Total time in hrs and mins.
  */
-function getMovieRuntime(minutes) {
-	return `${parseInt(minutes / 60)}h ${minutes % 60}m`;
+function getRuntime(minutes) {
+	return minutes ? `${parseInt(minutes / 60)}h ${minutes % 60}m` : '';
 }
 
-export { getMovieRuntime };
+export { getRuntime };
