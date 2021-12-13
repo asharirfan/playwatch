dda3fba41dadf77140102eddfd80defd597f6b88
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-14 02:20:56 +0500

    Improve single movie page

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,80 +1,140 @@
 import Layout from '@/components/common/Layout';
-import { getYear } from '@/functions/getDate';
-import { getMovie } from '@/functions/getMovie';
+import {
+	getMovie,
+	getMovieBackdropPath,
+	getMovieCredits,
+	getMovieExternalIds,
+	getMovieKeywords,
+	getMoviePosterPath
+} from '@/functions/getMovie';
 import { getAllTrendingMoviesByDay } from '@/functions/getTrendingMovies';
-import config from '@/utils/config';
 import Image from 'next/image';
-import { getMovieRuntime } from '@/functions/getRuntime';
+import Link from 'next/link';
+import { getPersonImgPath } from '@/functions/getPerson';
+import { formatCurrency } from '@/functions/format';
+import ProgressBar from '@ramonak/react-progress-bar';
+import Header from '@/components/common/single/Header';
 
 /**
  * Single movie page.
  *
- * @param  {object}  props       The component attributes as props.
- * @param  {object}  props.movie Movie object.
- * @return {Element}             Movie page.
+ * @param  {object}  props             The component attributes as props.
+ * @param  {object}  props.credits     Movie credits object.
+ * @param  {object}  props.externalIds Movie external ids object.
+ * @param  {object}  props.keywords    Movie keywords object.
+ * @param  {object}  props.movie       Movie object.
+ * @return {Element}                   Movie page.
  */
-export default function Movie({ movie }) {
+export default function Movie({ credits, externalIds, keywords, movie }) {
 	if (!movie) {
 		return <Layout>Movie not found.</Layout>;
 	}
 
-	const backdropPath = movie.backdrop_path
-		? `${config.tmdbImgBaseUrl}w1280${movie.backdrop_path}`
-		: 'https://via.placeholder.com/1280x720.png/353849/03CC90?text=PlayWatch';
-	const posterPath = movie.poster_path
-		? `${config.tmdbImgBaseUrl}w500${movie.poster_path}`
-		: 'https://via.placeholder.com/500x750.png/353849/03CC90?text=PlayWatch';
+	const { cast } = credits;
+
+	const backdropPath = getMovieBackdropPath(movie.backdrop_path);
+	const posterPath = getMoviePosterPath(movie.poster_path);
 
 	return (
 		<Layout>
-			<div className="min-h-headerBg">
-				<div
-					className="absolute bg-cover bg-center left-0 w-full h-headerBg"
-					style={{
-						backgroundImage: `url(${backdropPath})`,
-						zIndex: '-1'
-					}}
-				>
-					<div className="absolute top-0 h-headerBg w-full bg-darkGrey bg-opacity-80" />
+			<Header
+				backdropPath={backdropPath}
+				date={movie.release_date}
+				externalIds={externalIds}
+				genres={movie.genres}
+				overview={movie.overview}
+				posterPath={posterPath}
+				runTime={movie.runtime}
+				tagline={movie.tagline}
+				title={movie.title}
+			/>
+			<div className="flex mt-8">
+				<div className="w-4/5">
+					<h3 className="font-heading my-2 text-2xl">Cast</h3>
+					<div className="flex flex-wrap justify-start mt-4 -mx-2">
+						{cast &&
+							cast.map(person => {
+								if (!person.profile_path) {
+									return;
+								}
+
+								return (
+									<div className="w-36 m-2" key={person.id}>
+										<Link href={`/person/${person.id}`}>
+											<a className="cursor-pointer">
+												<Image
+													alt={person.name}
+													height={278}
+													src={getPersonImgPath(
+														person.profile_path,
+														'w185'
+													)}
+													width={185}
+												/>
+											</a>
+										</Link>
+										<Link href={`/person/${person.id}`}>
+											<a className="cursor-pointer text-lg hover:text-green">
+												<h4>{person.name}</h4>
+											</a>
+										</Link>
+										<p className="text-md opacity-60">
+											as {person.character}
+										</p>
+									</div>
+								);
+							})}
+					</div>
 				</div>
-				<div className="flex justify-between py-8">
-					<div>
-						<h3 className="font-heading text-4xl">{movie.title}</h3>
-						<p>
-							{getYear(movie.release_date)} -{' '}
-							{getMovieRuntime(movie.runtime)}
+				<div className="w-1/5 px-4 mt-2">
+					<div className="mb-4">
+						<p className="text-md mb-2 opacity-60">
+							Original Title
+						</p>
+						<p className="font-heading text-xl">
+							{movie.original_title}
 						</p>
 					</div>
-					<div>
-						<p>Popularity: {movie.vote_average}</p>
-					</div>
-				</div>
-				<div className="flex">
-					<div className="w-1/5">
-						<Image
-							alt={movie.title}
-							height="540"
-							src={posterPath}
-							width="360"
+					<div className="mb-4">
+						<p className="text-md mb-2 opacity-60">Popularity</p>
+						<ProgressBar
+							completed={movie.vote_average * 10}
+							bgColor="#03CC90"
+							baseBgColor="#ffffff"
+							labelColor="#3F4354"
 						/>
 					</div>
-					<div className="ml-4 w-4/5">
-						<p className="mb-2">
-							{movie.genres &&
-								movie.genres.map(genre => {
+					<div className="mb-4">
+						<p className="text-md mb-2 opacity-60">Status</p>
+						<p className="font-heading text-xl">{movie.status}</p>
+					</div>
+					<div className="mb-4">
+						<p className="text-md mb-2 opacity-60">Budget</p>
+						<p className="font-heading text-xl">
+							{formatCurrency(movie.budget)}
+						</p>
+					</div>
+					<div className="mb-4">
+						<p className="text-md mb-2 opacity-60">Revenue</p>
+						<p className="font-heading text-xl">
+							{formatCurrency(movie.revenue)}
+						</p>
+					</div>
+					<div className="mb-4">
+						<p className="text-md mb-2 opacity-60">Keywords</p>
+						<p>
+							{keywords.keywords &&
+								keywords.keywords.map(keyword => {
 									return (
 										<span
-											className="border inline-block mr-3 p-1 rounded-2xl"
-											key={genre.id}
+											className="bg-green inline-block mr-4 mb-4 py-1 px-2 text-grey rounded-2xl"
+											key={keyword.id}
 										>
-											{genre.name}
+											{keyword.name}
 										</span>
 									);
 								})}
 						</p>
-						<p className="italic mb-2">{movie.tagline}</p>
-						<h4 className="font-heading mb-2 text-xl">Overview</h4>
-						<p className="mb-2">{movie.overview}</p>
 					</div>
 				</div>
 			</div>
@@ -110,11 +170,12 @@ export async function getStaticPaths() {
  * @return {object}              Props object.
  */
 export async function getStaticProps({ params }) {
-	const movie = await getMovie(params.id);
-
 	return {
 		props: {
-			movie
+			credits: await getMovieCredits(params.id),
+			externalIds: await getMovieExternalIds(params.id),
+			keywords: await getMovieKeywords(params.id),
+			movie: await getMovie(params.id)
 		},
 		revalidate: 86400
 	};
