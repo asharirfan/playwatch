bed9973066fff38ebf7a57dd1cf529f173250adc
Ashar Irfan <mrasharirfan@gmail.com>
2021-12-15 01:24:12 +0500

    Improve footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,27 @@
+import Link from 'next/link';
+
 /**
  * Footer component.
  *
  * @return {Element} Footer.
  */
 export default function Footer() {
-	return <footer>Footer</footer>;
+	return (
+		<footer className="bg-grey">
+			<div className="container m-auto p-16 mt-8 text-center">
+				<Link href="/">
+					<a className="inline-block cursor-pointer">
+						<h3 className="font-heading text-4xl">PlayWatch</h3>
+					</a>
+				</Link>
+				<p className="mt-4 text-md">
+					This product uses{' '}
+					<Link href="https://www.themoviedb.org/">
+						<a className="cursor-pointer underline">TMDB</a>
+					</Link>{' '}
+					API but is not endorsed or certified by it.
+				</p>
+			</div>
+		</footer>
+	);
 }
